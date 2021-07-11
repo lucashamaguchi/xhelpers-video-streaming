bc15f44ad96f7a4c7cc7096945ea09413e3ac1df
lucashamaguchi <hamaguchi.lucas@gmail.com>
2021-07-11 18:33:40 -0300

    add account service logi

--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -1,5 +1,20 @@
+import axios from "axios";
+
 export default class AccountService {
-	async getApiUser(): Promise<any> {
-		return {};
+	private endpointLogin: string;
+	constructor() {
+		this.endpointLogin = `${process.env.ACCOUNT_API_URL}/api/auth`
+	}
+	async getApiUser(): Promise<{
+		email: string;
+		name: string;
+		token: string;
+	}> {
+		const payload = {
+			email: process.env.SERVICE_USER_EMAIL,
+			password: process.env.SERVICE_USER_PASSWORD,
+		};
+		const response = await axios.post(this.endpointLogin, payload);
+		return response.data;
 	}
 }
